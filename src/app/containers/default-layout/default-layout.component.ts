5cca8b933662a2553f0c03e09f69999f81137773
houssemEddineFeki <houssem.feki1@gmail.com>
2018-08-30 13:53:07 +0100

    read3

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -3,7 +3,8 @@ import { navItems } from './../../_nav';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './default-layout.component.html'
+  templateUrl: './default-layout.component.html',
+  styles: ['.phone { font-weight: 700;OpenSans, "Open Sans", sans-serif; font-size: 11px; line-height: 16px; color: #6e777d;}']
 })
 export class DefaultLayoutComponent {
   public navItems = navItems;
