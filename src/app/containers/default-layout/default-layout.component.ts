ccbfa41644bb48fb010bbc1c774452fb0ee244a7
houssemEddineFeki <houssem.feki1@gmail.com>
2018-09-01 13:52:32 +0100

    with-node5

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -8,17 +8,9 @@ import { navItems } from './../../_nav';
 })
 export class DefaultLayoutComponent {
   public navItems = navItems;
-  public sidebarMinimized = true;
-  private changes: MutationObserver;
-  public element: HTMLElement = document.body;
+
   constructor() {
 
-    this.changes = new MutationObserver((mutations) => {
-      this.sidebarMinimized = document.body.classList.contains('sidebar-minimized')
-    });
-
-    this.changes.observe(<Element>this.element, {
-      attributes: true
-    });
+   
+    }
   }
-}
